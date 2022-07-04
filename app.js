3c60796285bee311e64188e004ecc828fcd33a9f
kimmi1206 <kheinerimm1206@gmail.com>
2022-07-03 21:04:44 -0500

    adde auth controller

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors');
+
+// express
+const express = require('express');
+const app = express();
+
+// middleware modules
+const morgan = require('morgan');
+
+// database
+const connectDB = require('./db/connect');
+
+// Routers
+const authRouter = require('./routes/authRoutes');
+
+// Use Middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('e-commerce-api');
+});
+
+// Routes Middleware
+app.use('/api/v1/auth', authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// Server
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    const connection = await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
+const register = async (req, res) => {
+  console.log(req);
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+const login = async (req, res) => {
+  res.send('login user');
+};
+
+const logout = async (req, res) => {
+  res.send('logout user');
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
